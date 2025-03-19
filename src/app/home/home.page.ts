ba47bdf09499e24a36e64ff818ac46a8eaa33177
CaioGotardo <caiogotardo.dev@gmail.com>
2025-03-19 08:19:50 -0300

    asdada

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RangeCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,44 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+valorRange=0;
+medidasIniciais='';
+alertButtons=['ok'];
 
+medidasRadioFinal=''
+medidasFinais:number = 0 ;
+
+
+onIonChange(ev:RangeCustomEvent){
+  this.valorRange=parseInt(ev.detail.value.toString());
 }
+
+fazerConversao(){
+  if(this.medidasIniciais=='m' || this.medidasRadioFinal === 'km' ){
+  this.medidasFinais=this.valorRange/1000;
+    }
+    else if(this.medidasIniciais=='m' || this.medidasRadioFinal === 'm'){
+      this.medidasFinais=this.valorRange;
+
+    }
+    else if(this.medidasIniciais=='m' || this.medidasRadioFinal === 'milhas'){
+      this.medidasFinais=this.valorRange * 0.00062137;
+
+    }
+    else if(this.medidasIniciais === 'km'|| this.medidasRadioFinal === 'km'){
+this.valorRange=this.valorRange;
+
+    }else if(this.medidasIniciais === 'km'  || this.medidasRadioFinal === 'm'){
+      this.medidasFinais=this.valorRange*1000;
+    }else if(this.medidasIniciais === 'km' || this.medidasRadioFinal === 'milhas')
+{
+  this.medidasFinais==this.valorRange * 0,6214;
+}    }
+}
+
+
+
+
+
+
+
