559c7a42faff1e68671002f404353a5e9f0e39f9
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-19 07:35:49 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'exercicio02-range',
+  webDir: 'www'
+};
+
+export default config;
